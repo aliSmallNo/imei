fb5b71c92182ce57eadefb53195f0f96695f8be3
rain <eukitout@qq.com>
2017-05-26 14:18:01 +0800

    Rain

---
+++ admin/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'rgBvm6Ysutw5DlI1a0KKTdd1Xo5pGV6h',
+        ],
+    ],
+];
