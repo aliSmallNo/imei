5553de01d2c5417200c2e3675d2106477ecef7b0
rain <eukitout@qq.com>
2017-06-01 12:08:42 +0800

    Rain

--- mobile/views/layouts/imei.php
+++ mobile/views/layouts/imei.php
@@ -15,7 +15,7 @@
 		<link type="image/png" href="/favicon.png" rel="icon">
 		<link type="image/png" href="/favicon.png" rel="shortcut icon">
 		<link rel="stylesheet" href="/assets/css/layer.min.css?v=1.1.1">
-		<link rel="stylesheet" href="/css/imei.min.css?v=1.1.8">
+		<link rel="stylesheet" href="/css/imei.min.css?v=1.2.0">
 		<script src="/assets/js/jweixin-1.2.0.js"></script>
 	</head>
 	<body>
