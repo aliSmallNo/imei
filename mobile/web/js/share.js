946ed29fda829941f5edcca8aecf7ad7a7d463c2
rain <eukitout@qq.com>
2017-06-01 12:04:41 +0800

    Rain

--- mobile/web/js/share.js
+++ mobile/web/js/share.js
@@ -18,11 +18,56 @@ require(["layer"],
 			curFrag: "slink",
 			cork: $(".app-cork"),
 			wxString: $("#tpl_wx_info").html(),
+			celebs: $('#tpl_celebs').html(),
+			shade: $(".m-popup-shade"),
+			main: $(".m-popup-main"),
+			content: $(".m-popup-content"),
+			dl: $('.dl'),
 			newIdx: 0,
 			newsTimer: 0,
-			loading: 0
+			loading: 0,
 		};
 
+		$('.btn-share').on(kClick, function () {
+			var html = '<i class="share-arrow">点击菜单分享</i>';
+			$sls.main.show();
+			$sls.main.append(html);
+			$sls.shade.fadeIn(160);
+			setTimeout(function () {
+				$sls.main.hide();
+				$sls.main.find('.share-arrow').remove();
+				$sls.shade.fadeOut(100);
+			}, 2500);
+		});
+
+		$('.editable').on(kClick, function () {
+			var self = $(this);
+			var cid = self.attr('data-id');
+			toggle($sls.celebs);
+			$sls.content.find('[data-id=' + cid + ']').addClass('cur');
+		});
+
+		$(document).on(kClick, '.m-popup-options > a', function () {
+			var self = $(this);
+			var cid = self.attr('data-id');
+			$sls.dl.attr('data-id', cid);
+			$sls.dl.html(self.html());
+			toggle();
+		});
+
+		function toggle(content) {
+			var util = $sls;
+			if (content) {
+				util.main.show();
+				util.content.html(content).addClass("animate-pop-in");
+				util.shade.fadeIn(160);
+			} else {
+				util.content.removeClass("animate-pop-in");
+				util.main.hide();
+				util.content.html('');
+				util.shade.fadeOut(100);
+			}
+		}
 
 		function showMsg(title, sec) {
 			var delay = sec || 3;
