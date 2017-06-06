944e0576a0db7acda1da7cce86529ba4bd636cfe
rain <eukitout@qq.com>
2017-06-06 14:02:55 +0800

    Rain

--- mobile/config/params-local.php
+++ mobile/config/params-local.php
@@ -1,8 +1,4 @@
 <?php
 return [
-<<<<<<< HEAD
-	'openid' => 'oYDJew5EFMuyrJdwRrXkIZLU2c58'
-=======
 	'openid' => 'oYDJewx6Uj3xIV_-7ciyyDMLq8Wc',
->>>>>>> d59e93f8c163ace1358ec7d3beabc2e9a43f8b9d
 ];
