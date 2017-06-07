414c1be569c29f0ab66f35a3705be1eb5ee6321c
rain <eukitout@qq.com>
2017-06-07 12:09:02 +0800

    Rain

--- mobile/web/js/imei.js
+++ mobile/web/js/imei.js
@@ -23,6 +23,7 @@ require(["layer"],
 			code: $('.code'),
 			wxString: $("#tpl_wx_info").html(),
 			btnCode: $('.btn-code'),
+			change: $('.change'),
 			loading: 0,
 			counting: 0
 		};
@@ -200,8 +201,10 @@ require(["layer"],
 			$('.change, .btn-change').on(kClick, function () {
 				if ($sls.form.hasClass('single')) {
 					$sls.form.addClass('matcher').removeClass('single');
+					$sls.change.html('点击这里切换成「媒婆」身份');
 				} else {
 					$sls.form.addClass('single').removeClass('matcher');
+					$sls.change.html('点击这里切换成「单身」身份');
 				}
 			});
 
