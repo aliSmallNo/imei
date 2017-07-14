26f98c8ab4f6cd6f1adc3a76ce4aefbaeb5943c9
zhoup <zhoup@bpbhd.com>
2017-07-14 15:42:02 +0800

    ZP

--- mobile/controllers/ApiController.php
+++ mobile/controllers/ApiController.php
@@ -538,12 +538,12 @@ class ApiController extends Controller
 				];
 				*/
 
-//				$unionId = (isset($data["unionId"]) && $data["unionId"]) ? $data["unionId"] : '';
-//				if ($unionId && $info = UserWechat::findOne(["wUnionId" => $unionId])) {
-//					$data = $info->wOpenId;
-//				} else {
-//					$data = '';
-//				}
+				$unionId = (isset($data["unionId"]) && $data["unionId"]) ? $data["unionId"] : '';
+				if ($unionId && $info = UserWechat::findOne(["wUnionId" => $unionId])) {
+					$data = $info->wOpenId;
+				} else {
+					$data = '';
+				}
 				break;
 
 		}
