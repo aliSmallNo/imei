558fb4215077857d9cf03672143a87dc16401e00
zhoup <zhoup@bpbhd.com>
2017-07-19 17:06:10 +0800

    ZP

--- mobile/web/js/sreg.js
+++ mobile/web/js/sreg.js
@@ -211,7 +211,7 @@ require(["layer"],
 					data: JSON.stringify($sls.postData),
 				}, function (res) {
 					showMsg(res.msg);
-					alert(JSON.stringify(res.data));
+					//alert(JSON.stringify(res.data));
 					if (res.code == 0) {
 						setTimeout(function () {
 							location.href = "/wx/single";
