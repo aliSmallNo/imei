1c12802871b22c2587a9a39339885957b4bd7dab
rain <eukitout@qq.com>
2017-07-20 14:13:05 +0800

    Rain

--- common/models/UserBuzz.php
+++ common/models/UserBuzz.php
@@ -103,6 +103,7 @@ class UserBuzz extends ActiveRecord
 					$qrInfo = UserQR::findOne(["qId" => $eventKey]);
 					$debug .= $wxOpenId . "**" . $qrInfo["qOpenId"] . "**" . $qrInfo["qCategory"] . "**" . $qrInfo["qCode"];
 					$addResult = "";
+					AppUtil::logFile($qrInfo, 5, __FUNCTION__, __LINE__);
 					if (strlen($wxOpenId) > 6) {
 						$addResult = self::addRel($qrInfo["qOpenId"], $wxOpenId, UserNet::REL_QR_SCAN, $eventKey);
 					}
