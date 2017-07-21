f302e789493932c69fce31d174264fcfc801a2a8
zhoup <zhoup@bpbhd.com>
2017-07-21 15:00:23 +0800

    ZP

--- mobile/views/layouts/home.php
+++ mobile/views/layouts/home.php
@@ -15,7 +15,7 @@
 		<link type="image/png" href="/favicon.png" rel="icon">
 		<link type="image/png" href="/favicon.png" rel="shortcut icon">
 		<link rel="stylesheet" href="/assets/css/layer.css?v=1.1.1">
-		<link rel="stylesheet" href="/css/imei.css?v=1.1.4">
+		<link rel="stylesheet" href="/css/imei.css?v=1.1.5">
 		<script src="/assets/js/jweixin-1.2.0.js"></script>
 	</head>
 	<body style="background: #F4F4F4">
