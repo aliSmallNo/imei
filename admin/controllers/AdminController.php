00787b81cb15df80b7f850ec9bbebd0c529689fa
rain <eukitout@qq.com>
2017-07-26 12:09:41 +0800

    Rain

--- admin/controllers/AdminController.php
+++ admin/controllers/AdminController.php
@@ -68,18 +68,6 @@ class AdminController extends BaseController
 	public function actionUsers()
 	{
 
-		$conn = AppUtil::db();
-		$sql = 'select * from im_user where uRole=10 AND uGender>9 AND uNote=\'\' ';
-		$ret = $conn->createCommand($sql)->queryAll();
-		$count = 0;
-		foreach ($ret as $row) {
-			$res = UserTrans::addReward($row['uId'], UserTrans::CAT_NEW, $conn);
-			var_dump([$res, $row['uId']]);
-			$count++;
-		}
-		var_dump($count);
-
-
 		Admin::staffOnly();
 		$page = self::getParam("page", 1);
 		$name = self::getParam('name');
