126791efc2aafde690f3af47d3d8c83f52d6a1f7
rain <eukitout@qq.com>
2017-08-07 12:34:38 +0800

    Rain

--- common/models/ChatMsg.php
+++ common/models/ChatMsg.php
@@ -83,10 +83,11 @@ class ChatMsg extends ActiveRecord
 		$entity->save();
 		$cId = $entity->cId;
 
-		$sql = 'update im_chat_group set gFirstCId=:cid WHERE gId=:gid AND gFirstCId < 1';
+		$sql = 'update im_chat_group set gFirstCId=:cid,gAddedOn=now(),gAddedBy=:uid WHERE gId=:gid AND gFirstCId < 1';
 		$conn->createCommand($sql)->bindValues([
 			':cid' => $cId,
-			':gid' => $gid
+			':gid' => $gid,
+			':uid' => $senderId
 		])->execute();
 
 		$sql = 'update im_chat_group set gLastCId=:cid,gUpdatedOn=now(),gUpdatedBy=:uid WHERE gId=:gid';
@@ -205,54 +206,32 @@ class ChatMsg extends ActiveRecord
 		return $ret;
 	}
 
-	public static function chat($uId, $subUId, $page = 1, $pageSize = 20)
+	public static function chat($uId, $subUId, $page = 1, $pageSize = 40)
 	{
 		$limit = ' Limit ' . ($page - 1) * $pageSize . ',' . ($pageSize + 1);
 		$conn = AppUtil::db();
-		$sql = 'select * from 
-			(select cId,cContent,cSenderId,cReceiverId,cAddedOn, \'right\' as cDir
-			 from im_chat_msg WHERE cSenderId=:uid and cReceiverId=:suid
-			union
-			select cId,cContent,cSenderId,cReceiverId,cAddedOn, \'left\' as cDir
-			 from im_chat_msg WHERE cSenderId=:suid and cReceiverId=:uid) as t
-			 order by t.cAddedOn ' . $limit;
+		list($uid1, $uid2) = self::sortUId($uId, $subUId);
+		$sql = 'select u.uName as `name`, u.uThumb as avatar,
+			 m.cId as cid, m.cContent as content,m.cAddedOn,m.cAddedBy
+			 from im_chat_group as g 
+			 join im_chat_msg as m on g.gId=cGId
+			 join im_user as u on u.uId=m.cAddedBy
+			 WHERE g.gUId1=:id1 AND g.gUId2=:id2
+			 order by m.cAddedOn ' . $limit;
 		$chats = $conn->createCommand($sql)->bindValues([
-			':uid' => $uId,
-			':suid' => $subUId,
+			':id1' => $uid1,
+			':id2' => $uid2,
 		])->queryAll();
 		$nextPage = 0;
 		if ($chats && count($subUId) > $pageSize) {
 			array_pop($chats);
 			$nextPage = $page + 1;
 		}
-		$sql = 'select * from im_user WHERE uId=:uid or uId=:suid';
-		$ret = $conn->createCommand($sql)->bindValues([
-			':uid' => $uId,
-			':suid' => $subUId,
-		])->queryAll();
-		$users = [];
-		foreach ($ret as $row) {
-			$id = $row['uId'];
-			if (!isset($users[$id])) {
-				$users[$id] = [];
-			}
-			$users[$id]['name'] = $row['uName'];
-			$users[$id]['avatar'] = ImageUtil::getItemImages($row['uThumb'])[0];
-		}
-
-		foreach ($chats as $k => $row) {
-			$id = $row['cSenderId'];
-			$chats[$k]['url'] = 'javascript:;';
-			if ($row['cDir'] == 'left') {
-				$chats[$k]['url'] = '/wx/sh?id=' . AppUtil::encrypt($subUId);
-			}
-			foreach ($row as $col => $val) {
-				$chats[$k][strtolower(substr($col, 1))] = $val;
-				unset($chats[$k][$col]);
-			}
-			if (isset($users[$id])) {
-				$chats[$k] = array_merge($chats[$k], $users[$id]);
-			}
+		foreach ($chats as $k => $chat) {
+			$chats[$k]['avatar'] = ImageUtil::getItemImages($chat['avatar'])[0];
+			$chats[$k]['dir'] = ($uId == $chat['cAddedBy'] ? 'right' : 'left');
+			$chats[$k]['url'] = ($uId == $chat['cAddedBy'] ? 'javascript:;' : '/wx/sh?id=' . AppUtil::encrypt($subUId));
+			unset($chats[$k]['cAddedOn'], $chats[$k]['cAddedBy']);
 		}
 		return [$chats, $nextPage];
 	}
@@ -261,13 +240,25 @@ class ChatMsg extends ActiveRecord
 	{
 		$conn = AppUtil::db();
 		$limit = ' LIMIT ' . ($page - 1) * $pageSize . ',' . ($pageSize + 1);
-		$sql = 'select    
-				 (case when m.cSenderId=:uid then m.cReceiverId when m.cReceiverId=:uid then m.cSenderId end) as contactId, 
-				 max(m.cId) as msgId
-				 from im_chat_msg as m  
-				 WHERE (m.cReceiverId=:uid or m.cSenderId=:uid)
-				 group by contactId 
-				 order by msgId desc ' . $limit;
+		$sql = 'select * FROM (select    
+			 g.gUId2 as uid, 
+			 u.uName as `name`, u.uThumb as avatar,
+			 m.cId as cid,m.cContent as content,m.cAddedOn,m.cReadFlag,m.cAddedBy
+			 from im_chat_group as g 
+			  JOIN im_chat_msg as m on g.gId=m.cGId AND g.gLastCId=m.cId
+			  JOIN im_user as u on u.uId=g.gUId2
+			 WHERE g.gUId1=:uid  
+			 UNION 
+			 select    
+			 g.gUId1 as uid, 
+			 u.uName as `name`, u.uThumb as avatar, 
+			 m.cId as cid,m.cContent as content,m.cAddedOn,m.cReadFlag,m.cAddedBy
+			 from im_chat_group as g 
+			  JOIN im_chat_msg as m on g.gId=m.cGId AND g.gLastCId=m.cId
+			  JOIN im_user as u on u.uId=g.gUId1
+			 WHERE g.gUId2=:uid ) as t
+			 order by cAddedOn desc ' . $limit;
+
 		$contacts = $conn->createCommand($sql)->bindValues([
 			':uid' => $uId
 		])->queryAll();
@@ -276,47 +267,21 @@ class ChatMsg extends ActiveRecord
 			array_pop($contacts);
 			$nextPage = $page + 1;
 		}
-		$userIDs = array_column($contacts, 'contactId');
-		$msgIDs = array_column($contacts, 'msgId');
-		$sql = 'select * from im_user WHERE uId in (' . implode(',', $userIDs) . ')';
-		$ret = $conn->createCommand($sql)->queryAll();
-		$users = [];
-		foreach ($ret as $row) {
-			$users[$row['uId']] = [
-				'uid' => $row['uId'],
-				'encryptId' => AppUtil::encrypt($row['uId']),
-				'name' => $row['uName'],
-				'avatar' => ImageUtil::getItemImages($row['uThumb'])[0],
-			];
-		}
-		$sql = 'select * from im_chat_msg WHERE cId in (' . implode(',', $msgIDs) . ')';
-		$ret = $conn->createCommand($sql)->queryAll();
-		$contents = [];
-		foreach ($ret as $row) {
-			$readFlag = intval($row['cReadFlag']);
-			if ($uId == $row['cSenderId']) {
+		foreach ($contacts as $k => $contact) {
+			$readFlag = intval($contact['cReadFlag']);
+			if ($uId == $contact['cAddedBy']) {
 				$readFlag = 1;
 			}
-			$contents[$row['cId']] = [
-				'cid' => $row['cId'],
-				'content' => $row['cContent'],
-				'readflag' => $readFlag,
-				'dt' => AppUtil::prettyDate($row['cAddedOn']),
-			];
-		}
-		foreach ($contacts as $k => $contact) {
-			$contactId = $contact['contactId'];
-			$msgId = $contact['msgId'];
-			if (isset($users[$contactId])) {
-				$contacts[$k] = array_merge($contacts[$k], $users[$contactId]);
-			}
-			if (isset($contents[$msgId])) {
-				$contacts[$k] = array_merge($contacts[$k], $contents[$msgId]);
-			}
+			$contacts[$k]['readflag'] = $readFlag;
+			$contacts[$k]['dt'] = AppUtil::prettyDate($contact['cAddedOn']);
+			$contacts[$k]['encryptId'] = AppUtil::encrypt($contact['uid']);
+			$contacts[$k]['avatar'] = ImageUtil::getItemImages($contact['avatar'])[0];
+			unset($contacts[$k]['cAddedBy'],
+				$contacts[$k]['cAddedOn'],
+				$contacts[$k]['cReadFlag']);
 		}
 		return [$contacts, $nextPage];
 	}
-
 
 	public static function items($condStr, $page, $pageSize = 20)
 	{
@@ -370,7 +335,8 @@ class ChatMsg extends ActiveRecord
 	}
 
 
-	public static function reset(){
+	public static function reset()
+	{
 
 		$conn = AppUtil::db();
 		$sql = 'INSERT INTO im_chat_group(gUId1,gUId2,gRound)
@@ -397,23 +363,23 @@ class ChatMsg extends ActiveRecord
 				':rid' => $receiverId
 			])->execute();
 		}
-		$sql='UPDATE im_chat_group as g
+		$sql = 'UPDATE im_chat_group as g
 			 join (select min(cId) as minId,max(cId) as maxId,cGId from im_chat_msg WHERE cGId>0 GROUP BY cGId) as t 
 			 on t.cGId=g.gId
 			 set gFirstCId=minId,gLastCId=maxId';
 		$conn->createCommand($sql)->execute();
 
-		$sql='UPDATE im_chat_group as g 
+		$sql = 'UPDATE im_chat_group as g 
 			 	JOIN im_chat_msg as m on g.gFirstCId = m.cId 
 			 	SET g.gAddedBy=m.cSenderId, gAddedOn=m.cAddedOn';
 		$conn->createCommand($sql)->execute();
 
-		$sql='UPDATE im_chat_group as g 
+		$sql = 'UPDATE im_chat_group as g 
 			 	JOIN im_chat_msg as m on g.gLastCId = m.cId 
 			 	SET gUpdatedBy=m.cSenderId,gUpdatedOn=m.cAddedOn';
 		$conn->createCommand($sql)->execute();
 
-		$sql='UPDATE im_chat_msg set cAddedBy=cSenderId WHERE cAddedBy<2 ';
+		$sql = 'UPDATE im_chat_msg set cAddedBy=cSenderId WHERE cAddedBy<2 ';
 		$conn->createCommand($sql)->execute();
 
 	}
