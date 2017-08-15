bb10fb82e9071e3c6ca6106f73ba39c5ea23714f
rain <eukitout@qq.com>
2017-08-15 21:33:35 +0800

    Rain

--- common/models/User.php
+++ common/models/User.php
@@ -1117,8 +1117,12 @@ class User extends ActiveRecord
 			$nextPage = $page + 1;
 		}
 		$items = [];
-		$fields = ['password', 'phone', 'openid', 'addedon', 'updatedon', 'album', 'album_cnt', 'homeland', 'homeland_t', 'setting',
-			'cert', 'certdate', 'certimage', 'certnote', 'certstatus', 'certstatus_t', 'location', 'rankdate', 'ranktmp'];
+		$fields = ['password', 'phone', 'openid', 'addedon', 'updatedon', 'album', 'album_cnt', 'homeland', 'homeland_t',
+			'cert', 'certdate', 'certimage', 'certnote', 'certstatus', 'certstatus_t', 'location', 'rankdate', 'ranktmp',
+			'setting', 'rank', 'weight', 'weight_t', 'marital', 'marital_t', 'coord', 'diet', 'diet_t', 'pet', 'pet_t',
+			'birthyear', 'birthyear_t', 'alcohol', 'alcohol_t', 'rest', 'rest_t', 'fitness', 'fitness_t', 'hint',
+			'horos', 'horos_t', 'estate', 'estate_t', 'belief', 'belief_t', 'car', 'car_t', 'height', 'height_t',
+			'income', 'income_t'];
 		foreach ($ret as $row) {
 			$item = self::fmtRow($row);
 			$item['stat'] = UserNet::getStat($item['id']);
