dfce2cdb9921fcf23fa6945198a0be78df304d02
rain <eukitout@qq.com>
2017-08-16 12:30:14 +0800

    Rain

--- common/models/UserMsg.php
+++ common/models/UserMsg.php
@@ -328,7 +328,7 @@ class UserMsg extends ActiveRecord
 			])->execute();
 			$titles = array_column($item, 'title');
 			WechatUtil::templateMsg(WechatUtil::NOTICE_ROUTINE,
-				$uid, '微媒100每日简报', implode(';', $titles));
+				$uid, '微媒100每日简报', implode('；', $titles));
 		}
 
 	}
