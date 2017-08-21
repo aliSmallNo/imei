48165d3e4f3874ac2ae138c172310dde61c9233f
rain <eukitout@qq.com>
2017-08-21 18:35:40 +0800

    Rain

--- common/models/User.php
+++ common/models/User.php
@@ -1601,7 +1601,7 @@ class User extends ActiveRecord
 		if (isset($iResult["wSubscribe"]) && $iResult["wSubscribe"]) {
 			$A += 6;
 		}
-		$I = ($iResult["nUId"] > 0 ? 0 : 0.1) + $I4 + $I5;
+		$I = ($iResult["nUId"] > 0 ? 0 : 0.1) + $I4 + $I5 + 1;
 
 		// "区分系数（Distinguish) D=-D1/10+D2*10+D3"	 D1:注册年龄 D2:资料完整度指标(资料完成度大于90%取值。小于90%视为缺省) D3:待定
 		$D = -intval($row["age"]) / 10 + ($row["percent"] > 90 ? $row["percent"] / 100 : 0) * 10;
