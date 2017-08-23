475860c02cd88bc2db39846e1002d893bfc3cfb0
rain <eukitout@qq.com>
2017-08-23 15:22:04 +0800

    Rain

--- common/models/UserWechat.php
+++ common/models/UserWechat.php
@@ -331,7 +331,7 @@ class UserWechat extends ActiveRecord
 			$cmdUpdate2->bindValues([':openid' => $id])->execute();
 			$postData["user_list"][] = ["openid" => $id, "lang" => "zh_CN"];
 
-			if ($index > 90) {
+			if ($index > 80) {
 				$updateCount += $updateInfo($fields, $token, $postData, $cmdUpdate, $debug);
 				$postData = ["user_list" => []];
 				$index = 0;
