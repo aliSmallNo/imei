52d4c790287f9e8a145dc624c82147c9719c9bea
rain <eukitout@qq.com>
2017-09-05 09:31:07 +0800

    Rain

--- common/models/User.php
+++ common/models/User.php
@@ -1235,7 +1235,7 @@ class User extends ActiveRecord
 			$nextpage = 0;
 		}
 		//Rain: 不想展示太多页了
-		if ($nextpage > 10) {
+		if ($nextpage > 12) {
 			$nextpage = 0;
 		}
 		return ["data" => $result, "nextpage" => $nextpage, "condition" => $myFilter, 'page' => $page];
