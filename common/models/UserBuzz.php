9d0f80dedc4853e11d374fba5f007e027a805710
zhoup <zhoup@bpbhd.com>
2017-09-18 11:51:26 +0800

    zp

--- common/models/UserBuzz.php
+++ common/models/UserBuzz.php
@@ -183,7 +183,7 @@ class UserBuzz extends ActiveRecord
 							]
 						]);
 					} elseif ($content == "金秋送礼") {
-						if (!User::findOne(["uOpenId" => $toUsername])->uStatus) {
+						if (!User::findOne(["uOpenId" => $fromUsername])->uStatus) {
 							$contents = "尊敬的微媒100用户，您好，您的手机号还没有登录哦~<a href='https://wx.meipo100.com/wx/imei'>点我登录</a>查看活动。";
 						}else{
 							$contents = "新品iphone8,微媒送好礼。恭喜您获得参加此活动机会，动动手指参与吧.....<a href='https://wx.meipo100.com/wx/pin8'>点击了解活动详情</a>。";
