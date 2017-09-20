9fa7985f7c2e24a87bb88becc3d382d973e4aac6
rain <eukitout@qq.com>
2017-09-20 16:23:50 +0800

    Rain

--- console/controllers/FooController.php
+++ console/controllers/FooController.php
@@ -588,6 +588,6 @@ class FooController extends Controller
 	public function actionZp()
 	{
 		$ret = WechatUtil::createWechatMenus();
-		return $ret;
+		echo json_encode($ret, JSON_UNESCAPED_UNICODE);
 	}
 }
