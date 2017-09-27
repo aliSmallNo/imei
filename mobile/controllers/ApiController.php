8ef49a070d7f8b49534853d9953a5a77676a11b0
zhoup <zhoup@bpbhd.com>
2017-09-27 21:31:22 +0800

    z

--- mobile/controllers/ApiController.php
+++ mobile/controllers/ApiController.php
@@ -1604,12 +1604,13 @@ class ApiController extends Controller
 
 					$parseCode = BaiduUtil::postVoice($url);
 					if (mb_strpos($parseCode, $ling) !== false || 1) {
-						$aff = RedpacketList::Grap($rid, $uid, $url, $miao);
 						return self::renderAPI(129, 'test', [
 							"data" => $data,
 							"records" => $res,
-							"aff" => $aff,
+							"aff" => '',
 						]);
+						$aff = RedpacketList::Grap($rid, $uid, $url, $miao);
+
 						if ($aff) {
 							list($des, $follows) = Redpacket::rInfo($rid, $uid);
 							return self::renderAPI(0, '', [
