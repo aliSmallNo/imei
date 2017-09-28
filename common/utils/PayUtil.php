5f698ae3573ad2cb3702a0f28df791881b13666c
rain <eukitout@qq.com>
2017-09-28 14:40:41 +0800

    Rain

---
+++ common/utils/PayUtil.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: weirui
+ * Date: 28/9/2017
+ * Time: 2:22 PM
+ */
+
+namespace common\utils;
+
+
+class PayUtil
+{
+	public static function pack()
+	{
+
+	}
+}
