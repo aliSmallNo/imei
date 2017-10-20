a060f3b57de49bf86dfcfaaa52e6f7994f78f165
zhoup <zhoup@bpbhd.com>
2017-10-20 15:50:34 +0800

    zp

--- mobile/web/js/cert2.js
+++ mobile/web/js/cert2.js
@@ -55,7 +55,7 @@ require(["layer"],
 		function uploadImages() {
 			alert(JSON.stringify($sls.localId));
 			alert($sls.localId.length);
-			if (!$sls.localId || !$sls.localId.length < 2) {
+			if (!$sls.localId || $sls.localId.length < 2) {
 				showMsg("上传照片信息不全哦");
 				return;
 			}
