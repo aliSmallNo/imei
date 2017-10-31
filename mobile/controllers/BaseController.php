6c17d647e597c0499b79a9d82e4d03071327287a
rain <eukitout@qq.com>
2017-10-31 20:07:34 +0800

    Rain

--- mobile/controllers/BaseController.php
+++ mobile/controllers/BaseController.php
@@ -137,7 +137,7 @@ class BaseController extends Controller
 	protected function renderPage($view,
 	                              $params = [],
 	                              $layout = 'imei.php',
-	                              $title = '千寻恋恋-媒桂花飘香',
+	                              $title = '',
 	                              $bodyClass = '')
 	{
 		$this->layout = $layout;
@@ -158,7 +158,7 @@ class BaseController extends Controller
 			$params['wxInfoString'] = json_encode($sign);
 		}
 		if (!$title) {
-			$title = '千寻恋恋-媒桂花飘香';
+			$title = '千寻恋恋-缘来是你';
 		}
 		$appView = YII::$app->view;
 		$appView->params['page_head_title'] = $title;
