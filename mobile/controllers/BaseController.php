25b041e2190f69980ce86b4682c16a77e020bec7
zhoup <zhoup@bpbhd.com>
2017-12-19 19:41:36 +0800

    z

--- mobile/controllers/BaseController.php
+++ mobile/controllers/BaseController.php
@@ -20,7 +20,7 @@ class BaseController extends Controller
 	const ICON_OK_HTML = '<i class="fa fa-check-circle gIcon"></i> ';
 	const ICON_ALERT_HTML = '<i class="fa fa-exclamation-circle gIcon"></i> ';
 	const COOKIE_OPENID = "wx-openid";
-	const CSS_VERSION = '1.2.7.7';
+	const CSS_VERSION = '1.2.7.8';
 	static $WX_OpenId = "";
 
 	protected $user_id = 0;
