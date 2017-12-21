00701ce607be5e87341db57a1d7bf3fa97ab1e9d
zhoup <zhoup@bpbhd.com>
2017-12-21 17:25:25 +0800

    z

--- common/models/Order.php
+++ common/models/Order.php
@@ -59,20 +59,23 @@ class Order extends ActiveRecord
 
 		// 处理礼包内的商品
 		$uid = $entity->oUId;
+		$oNum = $entity->oNUm;
 		$gInfo = Goods::findOne(["gId" => $entity->oGId])->toArray();
 		if ($gInfo["gDesc"]) {
 			$desc = json_decode($gInfo["gDesc"], 1);
 			// 礼包商品
 			if (isset($desc["glsit"]) && $desc["glsit"]) {
 				foreach ($desc["glsit"] as $g) {
-					Order::add(["oUId" => $uid, "oGId" => $g["gid"], "oNum" => $g["num"], "oAmount" => 0, "oStatus" => self::ST_PAY, "oNote" => $oid]);
+					Order::add(["oUId" => $uid, "oGId" => $g["gid"], "oNum" => $g["num"] * $oNum, "oAmount" => 0, "oStatus" => self::ST_PAY, "oNote" => $oid]);
 				}
 			}
 			// 礼包卡(目前只有月卡赠送)
 			if (isset($desc["klsit"]) && $desc["klsit"]) {
 				foreach ($desc["klsit"] as $k) {
 					if ($k["cat"] == "chat_month") {
-						UserTag::addByPId(UserTag::CAT_CHAT_MONTH, $pid);
+						for ($i = 0; $i < $oNum; $i++) {
+							UserTag::addByPId(UserTag::CAT_CHAT_MONTH, $pid);
+						}
 					}
 				}
 			}
