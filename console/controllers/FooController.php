3bdd48c348e893d6dd8ce3ce58262d6ca6f17ddf
rain <eukitout@qq.com>
2017-12-25 20:41:49 +0800

    Rain

--- console/controllers/FooController.php
+++ console/controllers/FooController.php
@@ -792,16 +792,16 @@ class FooController extends Controller
 	{
 		$conn = AppUtil::db();
 		$dt = date('Y-m-d H:i:s', time() - 4000);
-		/*$sql = "SELECT uId,uGender
+		$sql = "SELECT uId,uGender
  				FROM im_user as u
  				JOIN im_user_wechat as w on w.wUId=u.uId AND w.wSubscribe=1
  				WHERE uGender>9 and uPhone!='' 
   					AND NOT EXISTS(SELECT 1 FROM im_chat_group WHERE gUId1=120000 AND gUId2=u.uId and gUpdatedOn>'$dt') order by uId ASC ";
-		$ret = $conn->createCommand($sql)->queryAll();*/
-		$ret[] = [
+		$ret = $conn->createCommand($sql)->queryAll();
+		/*$ret[] = [
 			'uId' => 131379,
 			'uGender' => 11,
-		];
+		];*/
 		$cnt = 0;
 		$senderId = User::SERVICE_UID;
 		foreach ($ret as $row) {
