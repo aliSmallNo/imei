517f76408115118e79040182c7d8dbc22f855972
zhoup <zhoup@bpbhd.com>
2018-01-29 18:53:31 +0800

    zp

--- mobile/web/js/zone.js
+++ mobile/web/js/zone.js
@@ -6,6 +6,7 @@ require(["jquery", "alpha"],
 			cork: $(".app-cork"),
 			wxString: $("#tpl_wx_info").html(),
 			wxUrl: $('#cWXUrl').val(),
+			curFrag: '',
 
 			shade: $(".m-popup-shade"),
 			main: $(".m-popup-main"),
@@ -13,6 +14,46 @@ require(["jquery", "alpha"],
 
 			loading: 0
 		};
+
+		var pageCommentsUtil = {
+			init: function () {
+				var util = this;
+				$(document).on(kClick, "[page_comments]", function () {
+					var self = $(this);
+
+					var tag = self.attr("page_comments");
+					switch (tag) {
+						case "entry":
+							var obj = $(".zone_container_item_comments_vbtns");
+							if (obj.hasClass("active")) {
+								obj.removeClass("active");
+							} else {
+								obj.addClass("active");
+							}
+							break;
+						case "send":
+							break;
+						case "voice":
+							util.changeRecord(self);
+							break;
+					}
+				});
+			},
+			changeRecord: function ($btn) {
+				var f = $btn.hasClass("play");
+				var span = $btn.closest(".vbtn_pause").find("p span");
+				if (f) {
+					$btn.removeClass("play").addClass("pause");
+					span.addClass("active");
+					span.html('01:23');
+				} else {
+					$btn.removeClass("pause").addClass("play");
+					span.removeClass("active");
+					span.html('点击录音');
+				}
+			},
+		};
+		pageCommentsUtil.init();
 
 		$(document).on(kClick, ".vip_mouth_gift a.btn", function () {
 			var self = $(this);
@@ -77,9 +118,43 @@ require(["jquery", "alpha"],
 			});
 		}
 
+		function locationHashChanged() {
+			var hashTag = location.hash;
+			hashTag = hashTag.replace("#!", "");
+			hashTag = hashTag.replace("#", "");
+			switch (hashTag) {
+				case 'zone_item':
+
+					break;
+				case 'zone_items':
+
+					break;
+				default:
+					break;
+			}
+			if (!hashTag) {
+				hashTag = 'index';
+			}
+			$sls.curFrag = hashTag;
+
+			var title = $("#" + hashTag).attr("data-title");
+			if (title) {
+				$(document).attr("title", title);
+				$("title").html(title);
+				var iFrame = $('<iframe src="/blank.html" class="g-blank"></iframe>');
+				iFrame.on('load', function () {
+					setTimeout(function () {
+						iFrame.off('load').remove();
+					}, 0);
+				}).appendTo($("body"));
+			}
+			alpha.clear();
+		}
+
 		$(function () {
 			var wxInfo = JSON.parse($sls.wxString);
 			wxInfo.debug = false;
+			window.onhashchange = locationHashChanged;
 			wxInfo.jsApiList = ['checkJsApi', 'hideOptionMenu', 'hideMenuItems', 'onMenuShareTimeline', 'onMenuShareAppMessage'];
 			wx.config(wxInfo);
 			wx.ready(function () {
@@ -96,6 +171,6 @@ require(["jquery", "alpha"],
 				});
 			});
 			$sls.cork.hide();
-
+			locationHashChanged();
 		});
 	});
