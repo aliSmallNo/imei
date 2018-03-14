ae751003488ebca12c63fa6263d3356299fe0837
rain <eukitout@qq.com>
2018-03-14 14:12:02 +0800

    Rain

--- common/models/UserBuzz.php
+++ common/models/UserBuzz.php
@@ -246,7 +246,7 @@ class UserBuzz extends ActiveRecord
 				break;
 			case '17':
 			case '早餐':
-				$txt='给大家争取了一个福利，免费包邮无限制送福利啦，雀巢奶粉送你营养早餐套装。 请点链接打开页面，滑动至页面中部，点击<a href="https://915.im/OrO7AR">立即领取试用装</a>';
+				$txt='给大家争取了一个福利，免费包邮无限制送福利啦，雀巢奶粉送你营养早餐套装🍽。 请点链接打开页面，滑动至页面中部，点击<a href="https://915.im/OrO7AR">立即领取试用装</a>';
 				$resp = self::textMsg($fromUsername, $toUsername, $txt);
 				break;
 			case '晚安':
