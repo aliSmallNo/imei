3ba4f65c836e0fa73b12d31e09b2ab220b764509
zhoup <zhoup@bpbhd.com>
2018-04-27 20:16:41 +0800

    zp

--- admin/models/Admin.php
+++ admin/models/Admin.php
@@ -361,7 +361,7 @@ class Admin extends ActiveRecord
 		if (!$adminId) {
 			$adminId = self::getAdminId();
 		}
-		return in_array($adminId, [1001, 1002, 1006, 1017, 1010]);
+		return in_array($adminId, [1001, 1002, 1006, 1017, 1010, 1026]);
 	}
 
 	public static function staffOnly()
