bd18bca9f51a45ffd4073212433af38a532f2588
zhoup <zhoup@bpbhd.com>
2018-06-05 18:13:43 +0800

    zp

--- common/models/YzUser.php
+++ common/models/YzUser.php
@@ -10,6 +10,7 @@ namespace common\models;
 use admin\models\Admin;
 use common\utils\AppUtil;
 use common\utils\ImageUtil;
+use common\utils\YouzanUtil;
 use yii\db\ActiveRecord;
 
 class YzUser extends ActiveRecord
@@ -60,6 +61,67 @@ class YzUser extends ActiveRecord
 		}
 		$entity->save();
 		return true;
+	}
+
+
+	/**
+	 * 获取指定时间段用户信息
+	 */
+	public static function getUserBySETime()
+	{
+
+		// 根据关注时间段批量查询微信粉丝用户信息
+		$st = '2018-03-27 13:36:58';
+		$et = '2018-06-05 23:59:59';
+		$page = 1;
+		$page_size = 20;
+		$days = floor((strtotime($et) - strtotime($st)) / 86400);
+
+		for ($d = 0; $d < $days; $d++) {
+			$stime = date('Y-m-d', strtotime($st) + $d * 86400);
+			$etime = date('Y-m-d', strtotime($st) + ($d + 1) * 86400);
+
+			$results = self::getTZUser($stime, $etime, $page, $page_size);
+			if ($results && $results['total_results'] > 0) {
+				$total_results = $results['total_results'];
+				$page_count = ceil($total_results / $page_size);
+
+				for ($i = 0; $i < $page_count; $i++) {
+					$users = self::getTZUser($stime, $etime, ($i + 1), $page_size)['users'];
+					foreach ($users as $v) {
+						$uid = $v['user_id'];
+						$insert = [];
+						foreach (YzUser::$fieldMap as $key => $val) {
+							if (isset($v[$key])) {
+								$insert[$val] = $v[$key];
+							}
+						}
+						$insert['uRawData'] = json_encode($v, JSON_UNESCAPED_UNICODE);
+						// echo $uid;print_r($insert);exit;
+						YzUser::edit($uid, $insert);
+					}
+				}
+			}
+		}
+	}
+
+	public function getTZUser($stime, $etime, $page, $page_size)
+	{
+		$method = 'youzan.users.weixin.followers.info.search';
+		$params = [
+			'page_no' => $page,
+			'page_size' => $page_size,
+			'start_follow' => $stime,
+			'end_follow' => $etime,
+			'fields' => 'points,trade,level',
+		];
+		$ret = YouzanUtil::getData($method, $params);
+		$results = $ret['response'] ?? 0;
+
+		AppUtil::logFile($results, 5, __FUNCTION__, __LINE__);
+		echo "stime:" . $stime . ' == etime:' . $etime . ' == ' . 'page:' . $page . ' == ' . 'pagesize:' . $page_size . PHP_EOL;
+		return $results;
+
 	}
 
 
--- console/controllers/FooController.php
+++ console/controllers/FooController.php
@@ -1423,75 +1423,18 @@ class FooController extends Controller
 	{
 		// $token = YouzanUtil::getAccessToken();
 
-		/*//获取当前店铺分销员列表，需申请高级权限方可调用。
+		//获取当前店铺分销员列表，需申请高级权限方可调用。
 		$method = 'youzan.salesman.accounts.get';
 		$params = [
 			'page_no' => 1,
-			'page_size' => 20,
+			'page_size' => 5,
 		];
-		YouzanUtil::getData($method, $params);
-		*/
+		$res = YouzanUtil::getData($method, $params);
+		print_r($res);
 
 
-		// 根据关注时间段批量查询微信粉丝用户信息
-		/*$conn = AppUtil::db();
-		$sql = "insert into im_yz_user (uYZUId,uOpenId,uRawData)
-				SELECT :yz_uid,:openid,:raw FROM dual
-				WHERE NOT EXISTS(SELECT 1 FROM im_yz_user as u WHERE u.uYZUId=:yz_uid ) ";
-		$insertCMD = $conn->createCommand($sql);*/
-
-
-		$st = '2018-03-27 13:36:58';
-		$et = '2018-06-05 23:59:59';
-		$page = 1;
-		$page_size = 20;
-		$days = floor((strtotime($et) - strtotime($st)) / 86400);
-
-		for ($d = 0; $d < $days; $d++) {
-			$stime = date('Y-m-d', strtotime($st) + $d * 86400);
-			$etime = date('Y-m-d', strtotime($st) + ($d + 1) * 86400);
-
-			$results = self::getTZUser($stime, $etime, $page, $page_size);
-			if ($results && $results['total_results'] > 0) {
-				$total_results = $results['total_results'];
-				$page_count = ceil($total_results / $page_size);
-
-				for ($i = 0; $i < $page_count; $i++) {
-					$users = self::getTZUser($stime, $etime, ($i + 1), $page_size)['users'];
-					foreach ($users as $v) {
-						$uid = $v['user_id'];
-						$insert = [];
-						foreach (YzUser::$fieldMap as $key => $val) {
-							if (isset($v[$key])) {
-								$insert[$val] = $v[$key];
-							}
-						}
-						$insert['uRawData'] = json_encode($v, JSON_UNESCAPED_UNICODE);
-						// echo $uid;print_r($insert);exit;
-						YzUser::edit($uid, $insert);
-					}
-				}
-			}
-		}
-	}
-
-	public function getTZUser($stime, $etime, $page, $page_size)
-	{
-		$method = 'youzan.users.weixin.followers.info.search';
-		$params = [
-			'page_no' => $page,
-			'page_size' => $page_size,
-			'start_follow' => $stime,
-			'end_follow' => $etime,
-			'fields' => 'points,trade,level',
-		];
-		$ret = YouzanUtil::getData($method, $params);
-		$results = $ret['response'] ?? 0;
-
-		AppUtil::logFile($results, 5, __FUNCTION__, __LINE__);
-		echo "stime:" . $stime . ' == etime:' . $etime . ' == ' . 'page:' . $page . ' == ' . 'pagesize:' . $page_size . PHP_EOL;
-
-		return $results;
+		// 根据指定时间段批量查询微信粉丝用户信息
+		// YzUser::getUserBySETime();
 
 	}
 
