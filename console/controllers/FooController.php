a676b35ee2418869d664c4fee425e1d9d50861af
zhoup <zhoup@bpbhd.com>
2018-06-06 10:59:03 +0800

    zp

--- console/controllers/FooController.php
+++ console/controllers/FooController.php
@@ -1469,10 +1469,10 @@ class FooController extends Controller
 
 	public function actionYz()
 	{
-		 $token = YouzanUtil::getAccessToken();echo $token;
+		 //$token = YouzanUtil::getAccessToken();echo $token;
 
 		// 根据指定时间段批量查询微信粉丝用户信息
-		// YzUser::getUserBySETime();
+		 YzUser::getUserBySETime();
 
 
 		/*$st = '2018-03-27 13:36:58';
