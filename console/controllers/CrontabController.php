d510da63e111e437b65513784d8b3758a1c44e0c
zhoup <zhoup@bpbhd.com>
2018-06-07 11:44:47 +0800

    zp

--- console/controllers/CrontabController.php
+++ console/controllers/CrontabController.php
@@ -90,11 +90,7 @@ class CrontabController extends Controller
 	public function actionYzuser()
 	{
 		// 更新有赞用户
-		/*$file = trim(AppUtil::logDir()) . '/' . 'youzan_user' . date('Y-m-d') . '.log';
-		file_put_contents($file, date('Y-m-d H:i:s') . PHP_EOL, 8);*/
-
 		AppUtil::logByFile(date('Y-m-d H:i:s'), 'youzan_user', __FUNCTION__, __LINE__);
-
 		YzUser::UpdateUser();
 
 	}
