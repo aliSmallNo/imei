e13a730c90461c3ec2b4f40926637a6cb7f377e0
zhoup <zhoup@bpbhd.com>
2018-06-19 12:38:27 +0800

    zp

--- console/controllers/FooController.php
+++ console/controllers/FooController.php
@@ -1500,10 +1500,10 @@ class FooController extends Controller
 			])->execute();
 		}*/
 
-		/*$st = '2018-03-26 00:00:00';
+		$st = '2018-03-26 00:00:00';
 		$et = date('Y-m-d 23:23:59');
 		YzGoods::get_goods_by_se_time($st, $et, 6, 1);
-		YzGoods::get_goods_by_se_time($st, $et, 1, 1);*/
+		YzGoods::get_goods_by_se_time($st, $et, 1, 1);
 		YzGoods::update_all_goods_desc(1);
 
 		//
