681e7eaddd1d58581726a28f98e2b6d93dd7affc
zhoup <zhoup@bpbhd.com>
2018-06-26 16:44:00 +0800

    zp

--- common/models/YzRefund.php
+++ common/models/YzRefund.php
@@ -137,8 +137,8 @@ class YzRefund extends ActiveRecord
 
 	public static function get_goods_by_se_time($isDebugger = false)
 	{
-		$st = '2018-04-26 00:00:00';
-		//$st = date("Y-m-d") . ' 00:00:00';
+		//$st = '2018-04-26 00:00:00';
+		$st = date("Y-m-d") . ' 00:00:00';
 		$et = date('Y-m-d 23:23:59');
 		$dates = YouzanUtil::cal_se_date($st, $et);
 		$total = 0;
