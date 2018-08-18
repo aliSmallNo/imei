bba7b91a5fb7575a953d5388ba58bcd51da7c1be
eukitout <eukitout@qq.com>
2018-08-18 18:08:58 +0800

    Ray

--- common/config/main.php
+++ common/config/main.php
@@ -1,15 +1,21 @@
 <?php
 
 return [
-	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-	'components' => [
-		'cache' => [
-			'class' => 'yii\caching\FileCache',
-		],
-		/*'session' => [
-			'class' => 'yii\web\DbSession',
-			'sessionTable' => 'im_session',
-			'timeout' => 50
-		],*/
-	],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        /*'session' => [
+    'class' => 'yii\web\DbSession',
+    'sessionTable' => 'im_session',
+    'timeout' => 50
+    ],*/
+    ],
+    'bootstrap' => ['gii'],
+    'modules' => [
+        'gii' => [
+            'class' => 'yii\gii\Module',
+        ],
+    ],
 ];
