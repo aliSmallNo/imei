5074afaa608ea3e16cf79c74b929e8adb59dab37
zhoup <zhoup@bpbhd.com>
2018-08-20 18:34:32 +0800

    zp

--- mobile/controllers/ApiController.php
+++ mobile/controllers/ApiController.php
@@ -3196,6 +3196,10 @@ class ApiController extends Controller
 		switch ($tag) {
 			case 'share'://  分享到朋友
 				$note = self::postParam('note');
+				if ($note == "/wx/cut_price") {
+					Log::add(['oCategory' => Log::CAT_USER_CUT_PRICE, 'oKey' => Log::KEY_TO_PEOPLE, 'oUId' => $uid]);
+					return;
+				}
 				$nId = UserNet::addShare($uid, $subUId, UserNet::REL_QR_SHARE, $note);
 				if ($note == '/wx/share28') {
 					list($data) = UserNet::s28ShareStat($uid);
@@ -3212,18 +3216,16 @@ class ApiController extends Controller
 						"key" => $coinCat,
 					]);
 				}*/
-				if ($note == "/wx/cut_price") {
-					Log::add(['oCategory' => Log::CAT_USER_CUT_PRICE, 'oKey' => Log::KEY_TO_PEOPLE, 'oUId' => $uid]);
-				}
 				break;
 			case 'moment':// 分享到朋友圈
 				$amt = 16;
 				$note = self::postParam('note');
-				if (!$subUId) {
-					$subUId = 120003;
-				}
 				if ($note == "/wx/cut_price") {
 					Log::add(['oCategory' => Log::CAT_USER_CUT_PRICE, 'oKey' => Log::KEY_TO_MOMENT, 'oUId' => $uid]);
+					return;
+				}
+				if (!$subUId) {
+					$subUId = 120003;
 				}
 				$nId = UserNet::addShare($uid, $subUId, UserNet::REL_QR_MOMENT, $note);
 				// 双旦活动
