4c607824050e462fd594653d9080331f1e0f6ff7
zhoup <zhoup@bpbhd.com>
2018-08-20 12:15:05 +0800

    zp

--- common/utils/NoticeUtil.php
+++ common/utils/NoticeUtil.php
@@ -369,7 +369,7 @@ class NoticeUtil
 		if ($bodyInfo['template_id'] == "acipK-tTIWO_Tkcp143ax0cbEsAEKKjylOYGOhXwTRw") {
 			$kf_msg = "待办事项提醒" . PHP_EOL . PHP_EOL .
 				$this->first . PHP_EOL .
-				"事项: " . $this->keywords['keyword1'] . PHP_EOL . PHP_EOL .
+				"事项: " . $bodyInfo['data']['keyword1'] . PHP_EOL . PHP_EOL .
 				"时间: " . date('Y-m-d H:i') . PHP_EOL . PHP_EOL .
 				"<a href='" . $bodyInfo['url'] . "'>点击这里查看吧~</a>" . PHP_EOL;
 
