0dfcbeee6e3953b26d29dc8ae2b8b7b95eeca96b
zhoup <zhoup@bpbhd.com>
2018-08-21 15:22:49 +0800

    zp

--- common/utils/WechatUtil.php
+++ common/utils/WechatUtil.php
@@ -1295,9 +1295,12 @@ class WechatUtil
 			$content = '【微信红包】恭喜发财，大吉大利                                                                                                    
 
 【88888元现金红包最后一天大派送】聊天立即获得现金大红包，先到先得送完为止🎉🎉🎉 👉<a href="https://wx.meipo100.com/wx/hi">点击链接</a>👈';
-			$content = '你的一位微信联系人在［千寻恋恋］上将你设置为“暗恋对象”。由于你未使用千寻恋恋，你的好友发送了微信通知。如果你也“暗恋”Ta，你们将配对成功。👉<a href="https://wx.meipo100.com/wx/hi">点击马上注册</a>👈';
+			$content = '你的一位微信联系人在［千寻恋恋］上将你设置为“暗恋对象”。
+			由于你未使用千寻恋恋，你的好友发送了微信通知。如果你也“暗恋”Ta，你们将配对成功。' . PHP_EOL .
+				'👉<a href="https://wx.meipo100.com/wx/hi">点击马上注册</a>👈';
 //			$ret = UserWechat::sendMsg($openIds, $content, $debug);
 //			$cnt = 0;
+			var_dump($openIds);
 			foreach ($openIds as $k => $openId) {
 
 				QueueUtil::loadJob('pushText',
