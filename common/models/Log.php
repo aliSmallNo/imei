8a1ec233b0a23a6538801896705bb2290cbb85f2
zhoup <zhoup@bpbhd.com>
2018-08-22 12:44:35 +0800

    zp

--- common/models/Log.php
+++ common/models/Log.php
@@ -657,7 +657,7 @@ class Log extends ActiveRecord
 			foreach ($res as $v) {
 				$uid = $v['oUId'];
 				$co = intval($v['co']);
-				$title = "好友点赞: " . $co . "次，剩余: " . self::CUT_TIMES - $co . "次";
+				$title = "好友点赞: " . $co . "次，剩余: " . (self::CUT_TIMES - $co) . "次";
 				WechatUtil::templateMsg(WechatUtil::NOTICE_CUT_PRICE_SUMMON, $uid, $title);
 			}
 		}
