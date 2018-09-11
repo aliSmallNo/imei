9223491512706702192d7ed52cc2b58c6cae15b0
zhoup <zhoup@bpbhd.com>
2018-09-11 17:23:52 +0800

    zp

--- console/controllers/FooController.php
+++ console/controllers/FooController.php
@@ -1629,7 +1629,7 @@ and `tDeletedFlag`=0 and DATEDIFF(`tExpiredOn`,now())>0 and tCategory=300";*/
 			])->execute();
 		}*/
 
-		YzOrders::Update_order('2018-08-24 00:00:00', '', 1);
+		YzOrders::Update_order('2018-09-05 00:00:00', '', 1);
 
 		//YzOrders::orders_user_mix_update();
 
