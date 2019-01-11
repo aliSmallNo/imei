18560e023e774605d14331107003564099667ed4
zhoup <zhoup@bpbhd.com>
2019-01-11 23:29:21 +0800

    zp

--- common/models/StockOrder.php
+++ common/models/StockOrder.php
@@ -117,8 +117,8 @@ class StockOrder extends ActiveRecord
 					'uName' => $value[1],
 				]);
 			} catch (\Exception $e) {
-				$error++;
 				Log::add(['oCategory' => Log::CAT_EXCEL, 'oKey' => $phone, 'oOpenId' => $value[1]]);
+				$error++;
 			}
 
 			if ($res) {
