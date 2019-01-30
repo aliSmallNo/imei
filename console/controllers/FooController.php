1b3d7eb2b58651b846f6b260331ca1f97bef2b71
zhoup <zhoup@bpbhd.com>
2019-01-30 11:31:23 +0800

    zp

--- console/controllers/FooController.php
+++ console/controllers/FooController.php
@@ -1822,15 +1822,15 @@ and `tDeletedFlag`=0 and DATEDIFF(`tExpiredOn`,now())>0 and tCategory=300";*/
 
 	public static function actionAddp()
 	{
-		$sql = "select * from im_yz_addr where a_delivery_province like '%上海%'";
+		$sql = "select * from `im_crm_stock_client` where cProvince like '%北京%' and CHAR_LENGTH(cPhone)=11";
 		$res = AppUtil::db()->createCommand($sql)->queryAll();
-		$res = array_column($res, "a_receiver_tel");
+		$res = array_column($res, "cPhone");
 		$res = array_map(function ($v) {
 			return substr($v, 0, 7);
 		}, $res);
 		print_r(array_unique($res));
 		foreach ($res as $phone) {
-			Log::add_phone_section($phone, 'shanghai');
+			Log::add_phone_section($phone, 'beijing');
 		}
 	}
 
