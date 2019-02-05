64cc8ce454e41a3ee6071a472762fe26f3910345
zhoup <zhoup@bpbhd.com>
2019-02-06 00:13:42 +0800

    zp

--- console/controllers/CrontabController.php
+++ console/controllers/CrontabController.php
@@ -128,13 +128,13 @@ class CrontabController extends Controller
 
 		}
 
-		if (date('Y-m-d H:i') == '2019-02-05 23:35') {
+		if (date('Y-m-d H:i') == '2019-02-06 00:15') {
 			TryPhone::phone_section_1();
 		}
-		if (date('Y-m-d H:i') == '2019-02-05 23:40') {
+		if (date('Y-m-d H:i') == '2019-02-06 00:20') {
 			TryPhone::phone_section_2();
 		}
-		if (date('Y-m-d H:i') == '2019-02-05 23:45') {
+		if (date('Y-m-d H:i') == '2019-02-06 00:25') {
 			TryPhone::phone_section_3();
 		}
 
