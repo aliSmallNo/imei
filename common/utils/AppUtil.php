a8992c70eab2cfcb467a5c881e8434129309421f
zhoup <zhoup@bpbhd.com>
2019-02-14 11:55:47 +0800

    zp

--- common/utils/AppUtil.php
+++ common/utils/AppUtil.php
@@ -662,7 +662,8 @@ class AppUtil
 	public static function check_encode($ret)
 	{
 		if (!mb_check_encoding($ret, 'utf-8')) {
-			$ret = mb_convert_encoding($ret, 'UTF-8', ['ASCII', 'UTF-8', 'GB2312', 'GBK', 'BIG5']);
+			//$ret = mb_convert_encoding($ret, 'UTF-8', ['ASCII', 'UTF-8', 'GB2312', 'GBK', 'BIG5']);
+			$ret = mb_convert_encoding($ret, 'UTF-8', 'ASCII,UTF-8,GB2312,GBK,BIG5');
 		}
 		return $ret;
 	}
