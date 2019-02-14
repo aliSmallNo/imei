82ba12c37d7bbac658efd22ea285322ecf9fd645
zhoup <zhoup@bpbhd.com>
2019-02-14 15:51:29 +0800

    zp

--- common/utils/AppUtil.php
+++ common/utils/AppUtil.php
@@ -661,9 +661,12 @@ class AppUtil
 
 	public static function check_encode($ret)
 	{
-		if (!mb_check_encoding($ret, 'utf-8')) {
-			$ret = mb_convert_encoding($ret, 'UTF-8', ['Unicode', 'ASCII', 'GB2312', 'GBK', 'JIS', "EUC-JP", "SJIS", 'UTF-8']);
-		}
+//		if (!mb_check_encoding($ret, 'utf-8')) {
+//			$ret = mb_convert_encoding($ret, 'UTF-8', ['Unicode', 'ASCII', 'GB2312', 'GBK', 'JIS', "EUC-JP", "SJIS", 'UTF-8']);
+//		}
+		$encoding = mb_detect_encoding($ret, "auto");
+		mb_substitute_character("none");
+		$ret = mb_convert_encoding($ret, 'UTF-8', $encoding);
 		return $ret;
 	}
 
