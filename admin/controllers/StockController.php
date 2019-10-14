8904ef0fe3cc5f67641086e64078546cad90c73c
zhoup <zhoup@bpbhd.com>
2019-10-14 09:17:58 +0800

    zp

--- admin/controllers/StockController.php
+++ admin/controllers/StockController.php
@@ -1373,7 +1373,7 @@ class StockController extends BaseController
         $data = AppUtil::json_decode($data);
         //echo count($data);exit;// 102093
         $header = ['手机号', '省', '市', 'type'];
-        ExcelUtil::getYZExcel('归属地' . date('Y-m-d'), $header, array_slice($data, 0, 20000));
+        ExcelUtil::getYZExcel('归属地' . date('Y-m-d'), $header, array_slice($data, 0, 10000));
 
         exit;
     }
