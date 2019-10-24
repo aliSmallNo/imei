0607247c5be10096223048f4362d5e5047dde9c8
zhoup <zhoup@bpbhd.com>
2019-10-24 11:27:22 +0800

    zp

--- common/models/StockTurn.php
+++ common/models/StockTurn.php
@@ -375,7 +375,7 @@ class StockTurn extends \yii\db\ActiveRecord
             $stockId = $v['mStockId'];
             $mCat = $v['mCat'];
             echo 'complete_lose_data:' . $stockId . PHP_EOL;
-            $lose_turn_list = StockTurn::find()->where(['tTurnover' => 0])->asArray()->all();
+            $lose_turn_list = StockTurn::find()->where(['tTurnover' => 0, 'tStockId' => $stockId])->asArray()->all();
             print_r($lose_turn_list);
             foreach ($lose_turn_list as $lose_turn) {
                 list($status, $hqs, $stat) = self::get_stock_turnover($stockId, $lose_turn['tTransOn'], $lose_turn['tTransOn']);
