73268014d7d4419f33049d6ad63101fe5f49d5fd
zhoup <zhoup@bpbhd.com>
2020-01-06 18:49:41 +0800

    zp

--- common/models/StockMainStat.php
+++ common/models/StockMainStat.php
@@ -263,8 +263,6 @@ class StockMainStat extends \yii\db\ActiveRecord
         $s_trans_on = $stat['s_trans_on'];                                  //
         $s_cat = $stat['s_cat'];                                            //
 
-        $sh_close_60avg_10avg_offset = StockMainTmp0::sh_close_60avg_10avg_offset($s_trans_on);
-
         $flag = false;
 
         $flag1 = intval($rule['r_stocks_gt']) != self::IGNORE_VAL ? $J_s_sh_change > $rule['r_stocks_gt'] : true;
@@ -290,10 +288,26 @@ class StockMainStat extends \yii\db\ActiveRecord
 
         $flag15 = intval($rule['r_scat']) ? in_array($s_cat, explode(',', $rule['r_scat'])) : true;
 
-        $flag16 = intval($rule['r_sh_close_60avg_10avg_offset_gt']) != self::IGNORE_VAL && $sh_close_60avg_10avg_offset != self::IGNORE_VAL
-            ? $sh_close_60avg_10avg_offset > $rule['r_sh_close_60avg_10avg_offset_gt'] : true;
-        $flag17 = intval($rule['r_sh_close_60avg_10avg_offset_lt']) != self::IGNORE_VAL && $sh_close_60avg_10avg_offset != self::IGNORE_VAL
-            ? $sh_close_60avg_10avg_offset < $rule['r_sh_close_60avg_10avg_offset_lt'] : true;
+//        $flag16 = intval($rule['r_sh_close_60avg_10avg_offset_gt']) != self::IGNORE_VAL && $sh_close_60avg_10avg_offset != self::IGNORE_VAL
+//            ? $sh_close_60avg_10avg_offset > $rule['r_sh_close_60avg_10avg_offset_gt'] : true;
+//        $flag17 = intval($rule['r_sh_close_60avg_10avg_offset_lt']) != self::IGNORE_VAL && $sh_close_60avg_10avg_offset != self::IGNORE_VAL
+//            ? $sh_close_60avg_10avg_offset < $rule['r_sh_close_60avg_10avg_offset_lt'] : true;
+
+        $flag16 = $flag17 = true;
+        if (intval($rule['r_sh_close_60avg_10avg_offset_gt']) != self::IGNORE_VAL) {
+            $sh_close_60avg_10avg_offset = StockMainTmp0::sh_close_60avg_10avg_offset($s_trans_on);
+            if ($sh_close_60avg_10avg_offset != self::IGNORE_VAL) {
+                $flag16 = $sh_close_60avg_10avg_offset > $rule['r_sh_close_60avg_10avg_offset_gt'];
+            }
+        }
+        if (intval($rule['r_sh_close_60avg_10avg_offset_lt']) != self::IGNORE_VAL) {
+            if (!isset($sh_close_60avg_10avg_offset)) {
+                $sh_close_60avg_10avg_offset = StockMainTmp0::sh_close_60avg_10avg_offset($s_trans_on);
+            }
+            if ($sh_close_60avg_10avg_offset != self::IGNORE_VAL) {
+                $flag17 = $sh_close_60avg_10avg_offset < $rule['r_sh_close_60avg_10avg_offset_lt'];
+            }
+        }
 
 
         switch ($rule['r_cat']) {
