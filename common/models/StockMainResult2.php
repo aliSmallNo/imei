ede298624362cdc70f966271a64bb6472d3ae50b
zhoup <zhoup@bpbhd.com>
2020-10-24 14:43:12 +0800

    zp

--- common/models/StockMainResult2.php
+++ common/models/StockMainResult2.php
@@ -897,6 +897,7 @@ class StockMainResult2 extends \yii\db\ActiveRecord
                 $f2 = array_merge($arr2[5], $arr2[10], $arr2[20], $arr2[60]);
                 if (!$f1 && !$f2 && $v['r_trans_on'] != date('Y-m-d')) {
                     unset($list[$k]);
+                    continue;
                 }
 
                 $list[$k]['f1'] = $list[$k]['f2'] = true;
