41b81d8b70b37b20ba8a0f53325623908984ec1b
zhoup <zhoup@bpbhd.com>
2020-11-20 15:07:54 +0800

    zp

--- common/models/StockMainResult2.php
+++ common/models/StockMainResult2.php
@@ -3485,8 +3485,8 @@ class StockMainResult2 extends \yii\db\ActiveRecord
 
         $trans = AppUtil::db()->beginTransaction();
 
-        //if (StockMain::is_trans_date() && time() > $stime && time() < $etime) {
-        if (StockMain::is_trans_date() || 1) {
+        if (StockMain::is_trans_date() && time() > $stime && time() < $etime) {
+//        if (StockMain::is_trans_date() || 1) {
             list($list_buy, $list_sold, $list_warn) = StockMainResult2::result_stat('', '');
             // 追加 平均收益率 期望收益率
             list($list, $rate_year_sum, $stat_rule_right_rate) = StockMainResult2::cal_back(StockMainPrice::TYPE_SH_CLOSE, 0, 0);
