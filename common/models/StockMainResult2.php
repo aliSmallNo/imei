4a195d639790d623e3f1277c6e9adbba5d357cc1
zhoup <zhoup@bpbhd.com>
2021-01-24 19:48:22 +0800

    zp

--- common/models/StockMainResult2.php
+++ common/models/StockMainResult2.php
@@ -3636,23 +3636,28 @@ class StockMainResult2 extends \yii\db\ActiveRecord
                 }
             }
         }
-        try {
-            foreach ($buy_data as $type => $item) {
-                $co = $item['yes'] + $item['no'] + $item['mid'];
 
-                $buy_data[$type]['yes_rate'] = $co ? sprintf('%.2f', $item['yes'] / $co) : 0;
-                $buy_data[$type]['no_rate'] = $co ? sprintf('%.2f', $item['no'] / $co) : 0;
-                $buy_data[$type]['mid_rate'] = $co ? sprintf('%.2f', $item['mid'] / $co) : 0;
-
-                $buy_data[$type]['rate_avg'] = $co ? sprintf('%.2f', $item['rate_sum'] / $co) : 0;
-                $buy_data[$type]['rule_co_avg'] = $co ? sprintf('%.2f', $item['rule_co_sum'] / $co) : 0;
+        foreach ($buy_data as $type => $item) {
+            if (!isset($item['yes']) || $item['no'] || $item['mid']) {
+                echo $type . "<br>";
+                print_r($item);
+                break;
+            } else {
+                echo $type . "<br>";
+                continue;
             }
-        } catch (\Exception $e) {
-            var_dump($type);
-            print_r($item);
-            exit;
-        }
 
+
+            $co = $item['yes'] + $item['no'] + $item['mid'];
+
+            $buy_data[$type]['yes_rate'] = $co ? sprintf('%.2f', $item['yes'] / $co) : 0;
+            $buy_data[$type]['no_rate'] = $co ? sprintf('%.2f', $item['no'] / $co) : 0;
+            $buy_data[$type]['mid_rate'] = $co ? sprintf('%.2f', $item['mid'] / $co) : 0;
+
+            $buy_data[$type]['rate_avg'] = $co ? sprintf('%.2f', $item['rate_sum'] / $co) : 0;
+            $buy_data[$type]['rule_co_avg'] = $co ? sprintf('%.2f', $item['rule_co_sum'] / $co) : 0;
+        }
+        exit;
 
         return $buy_data;
 
